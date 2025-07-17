9396dca621f2c00ef58cf2fc689fd1e09fe73a85
Ezuchu <jesusmiguelrr17@gmail.com>
2025-07-17 16:58:03 -0400

    botones cancelar y guardar

---
+++ condominio-app/src/modules/auth/components/botonGuardar.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="btnGuardar">
+        Guardar
+    </button>
+</template>
+
+<style lang="scss">
+    
+    .btnGuardar
+    {
+        background-color: var(--color-peligro);
+        color: var(--color-fondo);
+        border: 3px solid var(--color-peligro);
+        border-radius: 40px;
+
+        padding: 12px 40px;
+
+        min-height: 42px;
+        min-width:  42px;
+
+        font-weight: 600;
+
+        &:hover {
+            background-color: var(--color-fondo);
+            border-color: var(--color-peligro);
+            color: var(--color-peligro);
+        }
+    }
+</style>
